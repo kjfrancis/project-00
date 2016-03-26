cd316281a26b515a60d98bcc842a49ebbdd04241
kjfrancis <kef2010@gmail.com>
2016-03-26 15:28:02 -0700

    Starting to make progress1

--- script/javascript.js
+++ script/javascript.js
@@ -1,20 +1,40 @@
 $(document).on('ready', function() {
 
+console.log('JS is WORKING');
 
+var playerOne = "P1";
+var playerTwo = "P2";
 
+var resetGame = function () {
+  $boxes.text("");
+  $boxes.removeClass("X");
+  $boxes.removeClass("O");
+};
 
+/*$(window).keydown(function (event){
 
+    //move player one to the right
 
+});*/
 
+$(document).keydown(function (e){
 
+alert(e.keyCode);
+    //move player two to the right
 
+});
 
-
-
-
-
-
-
-
+$ ('#reset').on('click', function () {
+  console.log("Button got clicked");
+  resetGame();
+});
+/*$boxes.on('click', function() {
+  if ($(this).text() === "") {
+    $(this).text (turn);
+      changeTurn();
+    $(this).addClass(turn);
+    moves += 1;
+  }
+*/
 
 });
